c1fdc2e10f846465eac6a0be74c6bb8082a18501
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2014-07-10 16:10:03 -0600

    jenkins setup common code

---
+++ jenkins_scripts/distro_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+echo "clean workspace"
+cd "$WORKSPACE" && rm -rf *
+
+echo "untar the distribution"
+tar zxf $JENKINS_HOME/distro/running/$VERSION/$TIMESTAMP/dotcms_*.tar.gz
+
+# link tomcat-version to just tomcat
+ln -s dotserver/`ls dotserver | grep  tomcat` dotserver/tomcat
+
+echo "copy the jar with the tests"
+cp $JENKINS_HOME/distro/running/$VERSION/$TIMESTAMP/dotcms_tests_*.jar dotserver/tomcat/webapps/ROOT/WEB-INF/lib
+
+echo "copy license"
+mkdir -p dotserver/tomcat/webapps/ROOT/assets/license
+cp $JENKINS_HOME/dotcms_license.dat dotserver/tomcat/webapps/ROOT/assets/license/license.dat
+
+echo "uncomment test servlet"
+sed -i '/TEST FRAMEWORK SERVLETS/d' dotserver/tomcat/webapps/ROOT/WEB-INF/web.xml
+
+echo "build-tests.xml"
+jar xf dotserver/tomcat/webapps/ROOT/WEB-INF/lib/dotcms_tests_*.jar build-tests.xml
+
+echo "se es cluster name"
+sed -i 's/dotCMSContentIndex/$ESCLUSTER/g' dotserver/tomcat/webapps/ROOT/WEB-INF/classes/dotmarketing-config.properties
+

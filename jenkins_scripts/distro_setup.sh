cf372767ff78614d2092a52221b380eefb650ff6
Oscar Arrieta <oscar.arrieta@dotcms.com>
2014-12-09 13:53:06 -0600

    dotCMS/core#6785: No need to copy license

--- jenkins_scripts/distro_setup.sh
+++ jenkins_scripts/distro_setup.sh
@@ -14,11 +14,6 @@ mv dotserver/`ls dotserver | grep  tomcat` dotserver/tomcat
 echo "copy the jar with the tests"
 cp $JENKINS_HOME/distro/running/$VERSION/$TIMESTAMP/dotcms_tests_*.jar dotserver/tomcat/webapps/ROOT/WEB-INF/lib
 
-echo "copy license"
-#mkdir -p dotserver/tomcat/webapps/ROOT/assets/license
-#cp $JENKINS_HOME/dotcms_license.dat dotserver/tomcat/webapps/ROOT/assets/license/license.dat
-cp $JENKINS_HOME/trial.jsp dotserver/tomcat/webapps/ROOT/trial.jsp
-
 echo "uncomment test servlet"
 sed -i '/TEST FRAMEWORK SERVLETS/d' dotserver/tomcat/webapps/ROOT/WEB-INF/web.xml
 

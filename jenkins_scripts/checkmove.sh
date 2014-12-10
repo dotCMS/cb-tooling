1681ab36b36f598340fb69e8f352535b3a366d47
Oscar Arrieta <oscar.arrieta@dotcms.com>
2014-12-10 08:38:17 -0600

    dotCMS/core#6785: Adding Checkmove Bash file

---
+++ jenkins_scripts/checkmove.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION="$1"
+TIMESTAMP="$2"
+DISTRO=` cd $( dirname ${BASH_SOURCE[0]} ) && pwd `
+
+DIR="$DISTRO/running/$VERSION/$TIMESTAMP"
+if [ -f $DIR/pg_build ] && [ -f $DIR/my_build ] && [ -f $DIR/h2_build ] && [ -f $DIR/ora_build ] && [ -f $DIR/ms_build ]; then
+
+  echo "all unit tests done. moving $DISTRO/running/$VERSION/$TIMESTAMP to $DISTRO/ready/$VERSION/$TIMESTAMP"
+  mkdir -p $DISTRO/ready/$VERSION/
+  if [ -d $DISTRO/ready/$VERSION/$TIMESTAMP ]; then
+    rm -rf $DISTRO/ready/$VERSION/$TIMESTAMP
+  fi
+  mv $DISTRO/running/$VERSION/$TIMESTAMP $DISTRO/ready/$VERSION/
+
+else
+
+  echo "unit tests still not done"
+
+fi
+
+

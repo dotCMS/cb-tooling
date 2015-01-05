de7ebc9daf25dd0504026ba7ccfa08a17d8dda68
Oscar Arrieta <oscar.arrieta@dotcms.com>
2015-01-05 14:53:53 -0600

    Not waiting to all jobs finish to create a ready folder

--- jenkins_scripts/checkmove.sh
+++ jenkins_scripts/checkmove.sh
@@ -5,9 +5,9 @@ TIMESTAMP="$2"
 DISTRO=` cd $( dirname ${BASH_SOURCE[0]} ) && pwd `
 
 DIR="$DISTRO/running/$VERSION/$TIMESTAMP"
-if [ -f $DIR/pg_build ] && [ -f $DIR/my_build ] && [ -f $DIR/h2_build ] && [ -f $DIR/ora_build ] && [ -f $DIR/ms_build ]; then
+if [ -f $DIR/pg_build ] || [ -f $DIR/my_build ] || [ -f $DIR/h2_build ] || [ -f $DIR/ora_build ] || [ -f $DIR/ms_build ]; then
 
-  echo "all unit tests done. moving $DISTRO/running/$VERSION/$TIMESTAMP to $DISTRO/ready/$VERSION/$TIMESTAMP"
+  echo "At least 1 unit tests is done. moving $DISTRO/running/$VERSION/$TIMESTAMP to $DISTRO/ready/$VERSION/$TIMESTAMP"
   mkdir -p $DISTRO/ready/$VERSION/
   if [ -d $DISTRO/ready/$VERSION/$TIMESTAMP ]; then
     rm -rf $DISTRO/ready/$VERSION/$TIMESTAMP

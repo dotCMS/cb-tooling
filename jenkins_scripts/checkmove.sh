ab048bf7d3c0301de4aec78aba34d856b5cd8efa
Oscar Arrieta <oscar.arrieta@dotcms.com>
2015-01-06 10:35:10 -0600

    Updating to put ready under WORKSPACE

--- jenkins_scripts/checkmove.sh
+++ jenkins_scripts/checkmove.sh
@@ -8,11 +8,11 @@ DIR="$DISTRO/running/$VERSION/$TIMESTAMP"
 if [ -f $DIR/pg_build ] || [ -f $DIR/my_build ] || [ -f $DIR/h2_build ] || [ -f $DIR/ora_build ] || [ -f $DIR/ms_build ]; then
 
   echo "At least 1 unit tests is done. moving $DISTRO/running/$VERSION/$TIMESTAMP to $DISTRO/ready/$VERSION/$TIMESTAMP"
-  mkdir -p $DISTRO/ready/$VERSION/
+  mkdir -p $WORKSPACE/ready/$VERSION/
   if [ -d $DISTRO/ready/$VERSION/$TIMESTAMP ]; then
     rm -rf $DISTRO/ready/$VERSION/$TIMESTAMP
   fi
-  mv $DISTRO/running/$VERSION/$TIMESTAMP $DISTRO/ready/$VERSION/
+  mv $DISTRO/running/$VERSION/$TIMESTAMP $WORKSPACE/ready/$VERSION/
 
 else
 

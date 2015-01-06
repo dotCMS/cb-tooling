70061a2260a33027c535ba77da6155cf86ef5645
Oscar Arrieta <oscar.arrieta@dotcms.com>
2015-01-06 10:35:39 -0600

    fix Typo

--- jenkins_scripts/checkmove.sh
+++ jenkins_scripts/checkmove.sh
@@ -7,7 +7,7 @@ DISTRO="/var/lib/jenkins/distro"
 DIR="$DISTRO/running/$VERSION/$TIMESTAMP"
 if [ -f $DIR/pg_build ] || [ -f $DIR/my_build ] || [ -f $DIR/h2_build ] || [ -f $DIR/ora_build ] || [ -f $DIR/ms_build ]; then
 
-  echo "At least 1 unit tests is done. moving $DISTRO/running/$VERSION/$TIMESTAMP to $DISTRO/ready/$VERSION/$TIMESTAMP"
+  echo "At least 1 unit tests is done. moving $DISTRO/running/$VERSION/$TIMESTAMP to $WORKSPACE/ready/$VERSION/$TIMESTAMP"
   mkdir -p $WORKSPACE/ready/$VERSION/
   if [ -d $DISTRO/ready/$VERSION/$TIMESTAMP ]; then
     rm -rf $DISTRO/ready/$VERSION/$TIMESTAMP

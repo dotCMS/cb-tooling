75057434e43d3eaefa969ad671a53092c7f0cb3a
Anibal Gomez <anibal.gomez@dotcms.com>
2017-05-12 14:33:00 -0600

    dotCMS/core#11196: Added Jenkins scripts to build master

---
+++ jenkins_scripts/branches/master/nightly_actual_build.sh
@@ -0,0 +1,49 @@
+echo "**** ENV ****"
+env
+echo "**** ENV ****"
+
+cd "$WORKSPACE"
+
+rm -rf "$WORKSPACE/repo/dotCMS/build/"
+rm -rf "$WORKSPACE/repo/dist-output"
+
+cd "$WORKSPACE/repo/dotCMS"
+
+#Cleaning up gradle cache
+rm -rf "$WORKSPACE/repo/dotCMS/.gradle/"
+
+./gradlew clean --no-daemon --refresh-dependencies
+./gradlew createDist --no-daemon
+
+#VERSION=$(grep dotcms.release.version  src/main/webapp/WEB-INF/classes/release.properties | awk -F = '{ print $2 }')
+VERSION='master'
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+
+DISTRO="/var/lib/jenkins/distro"
+
+DIR="$WORKSPACE/repo/dist-output"
+
+rm -rf $DISTRO/ready/$VERSION/$TIMESTAMP
+mkdir -p $DISTRO/ready/$VERSION/$TIMESTAMP
+mkdir -p $DISTRO/ready/${GIT_BRANCH#*/}
+
+if [ -f $DIR/dotcms_*.zip ]; then
+
+	echo "Distribution Created. Moving $WORKSPACE/repo/dist-output to $DISTRO/ready/$VERSION/$TIMESTAMP"
+    if [ -d $DISTRO/ready/$VERSION/$TIMESTAMP ]; then
+    	rm -rf $DISTRO/ready/$VERSION/$TIMESTAMP
+  	fi
+    mv $DIR/dotcms_*.zip $DIR/dotcms_master.zip
+    mv $DIR/dotcms_*.tar.gz $DIR/dotcms_master.tar.gz
+	mv $WORKSPACE/repo/dist-output/* $DISTRO/ready/$VERSION/
+    echo "DOTCMS_BUILD_VERSION=$VERSION" > $DISTRO/ready/${GIT_BRANCH#*/}/params
+else
+
+	echo "Distribution still not created"
+fi
+
+echo "$GIT_COMMIT" > $JENKINS_HOME/distro/ready/$VERSION/commit
+echo "master" > $JENKINS_HOME/distro/ready/$VERSION/branch
+
+echo "TIMESTAMP=$TIMESTAMP"
+echo "VERSION=$VERSION"
---
+++ jenkins_scripts/branches/master/slave_post_build.sh
@@ -0,0 +1,26 @@
+export AWS_CREDENTIAL_PROFILES_FILE=$JENKINS_HOME/credentials
+export AWS_CREDENTIAL_PRIVATE_KEY_FILE=$JENKINS_HOME/dotcms-dev-test-deploy-2017-02-0x5513.pem
+
+export VERSION='master'
+export TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+
+
+# Terminate and delete AWS instance
+cd "$WORKSPACE/repo/dotCMS"
+if [ -f aws-ec2-instance-id.txt ]; then
+	export AWS_EC2_INSTANCE_ID=$(cat aws-ec2-instance-id.txt)
+    ./gradlew -b build-aws-tests.gradle terminateInstance -PinstanceId=$AWS_EC2_INSTANCE_ID -Pdatabase=$DOT_CMS_DATABASE_TYPE -Pbranch=${GIT_BRANCH#*/} -Pcommit=$GIT_COMMIT -PkeyFile=$AWS_CREDENTIAL_PRIVATE_KEY_FILE --no-daemon
+fi
+if [ -f aws-rds-instance-id.txt ]; then
+	export AWS_RDS_INSTANCE_ID=$(cat aws-rds-instance-id.txt)
+    ./gradlew -b build-aws-tests.gradle deleteDBInstance -PdbInstanceId=$AWS_RDS_INSTANCE_ID -Pdatabase=$DOT_CMS_DATABASE_TYPE -Pbranch=${GIT_BRANCH#*/} -Pcommit=$GIT_COMMIT -PkeyFile=$AWS_CREDENTIAL_PRIVATE_KEY_FILE --no-daemon
+fi
+
+
+# Output build url
+echo "VERSION="$VERSION "\nTIMESTAMP="$TIMESTAMP
+
+mkdir -p $JENKINS_HOME/distro/ready/$VERSION/$DOT_CMS_DATABASE_TYPE/$TIMESTAMP
+
+echo "write build url"
+echo "$BUILD_URL" > $JENKINS_HOME/distro/ready/$VERSION/$DOT_CMS_DATABASE_TYPE/$TIMESTAMP/$DOT_CMS_DATABASE_TYPE_build

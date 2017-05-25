902415601a37b96e11c34951b2acf288a8fdbf80
Anibal Gomez <anibal.gomez@dotcms.com>
2017-05-25 14:03:31 -0600

    dotCMS/core#11196/dotCMS/core#11197: Checking for new commits on Jenkins/AWS scripts for continuous builds

--- jenkins_scripts/builds/continuous/slave_actual_build.sh
+++ jenkins_scripts/builds/continuous/slave_actual_build.sh
@@ -15,38 +15,46 @@ cd "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS"
 sed -i "s,^org.gradle.jvmargs=,#org.gradle.jvmargs=,g" gradle.properties
 
 
-export CONTINOUS_AWS_EC2_INSTANCE_ID=$JENKINS_HOME/continuous/aws-ec2-instance-id.txt
+export CONTINUOUS_AWS_EC2_INSTANCE_ID=$JENKINS_HOME/continuous/aws-ec2-instance-id.txt
+export CONTINUOUS_GIT_COMMIT_ID=$JENKINS_HOME/continuous/git-commit-id-${GIT_BRANCH_NAME}.txt
 
 export DOTCMS_DATABASE_NAME=Postgres
 
 
-# A new AWS instance is created if needed 
-if [ -f $CONTINOUS_AWS_EC2_INSTANCE_ID ]; then
+if [ -f $CONTINUOUS_AWS_EC2_INSTANCE_ID ]; then
 
-	echo "Building ${GIT_BRANCH_NAME} and testing against ${DOTCMS_DATABASE_NAME}"
+	if [ "$CONTINUOUS_GIT_COMMIT_ID" == "$GIT_COMMIT" ]; then
 
-	./gradlew clean --no-daemon --refresh-dependencies 
+    	echo "Nothing to do. No new commit"
 
-	export AWS_EC2_INSTANCE_ID=$(cat $CONTINOUS_AWS_EC2_INSTANCE_ID)
+	else
+		echo "Building ${GIT_BRANCH_NAME} and testing against ${DOTCMS_DATABASE_NAME}"
+
+		./gradlew clean --no-daemon --refresh-dependencies 
+
+		export AWS_EC2_INSTANCE_ID=$(cat $CONTINUOUS_AWS_EC2_INSTANCE_ID)
 
 
-	# Run tests on remote AWS EC2 instance
-	./gradlew -b build-aws-tests.gradle executeScript -PpropertiesFile=build-aws-tests-continuous.properties -Pbranch=${GIT_BRANCH_NAME} -Pdatabase=$DOTCMS_DATABASE_NAME -PkeyFile=$AWS_CREDENTIAL_PRIVATE_KEY_FILE -PoutputFile=build-aws-tests-${GIT_BRANCH_NAME}.zip -Pprovisioned=true -PinstanceId=$AWS_EC2_INSTANCE_ID --no-daemon
+		# Run tests on remote AWS EC2 instance
+		./gradlew -b build-aws-tests.gradle executeScript -PpropertiesFile=build-aws-tests-continuous.properties -Pbranch=${GIT_BRANCH_NAME} -Pdatabase=$DOTCMS_DATABASE_NAME -PkeyFile=$AWS_CREDENTIAL_PRIVATE_KEY_FILE -PoutputFile=build-aws-tests-${GIT_BRANCH_NAME}.zip -Pprovisioned=true -PinstanceId=$AWS_EC2_INSTANCE_ID --no-daemon
 
 
-	# Uncompress tests results
-	cd "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build"
-	unzip build-aws-tests-${GIT_BRANCH_NAME}.zip
+		# Uncompress tests results
+		cd "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build"
+		unzip build-aws-tests-${GIT_BRANCH_NAME}.zip
 
-	# Print logs to console
-	cat "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build/tests/logs/dotcms.log"
+		# Print logs to console
+		cat "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build/tests/logs/dotcms.log"
 
-	# Saving tomcat logs into the build folder
-	mkdir -p "$WORKSPACE/logs/${BUILD_NUMBER}"
-	mv "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build/tests/logs"/* "$WORKSPACE/logs/${BUILD_NUMBER}/"
-	rm -r "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build/tests/logs/"
+		# Saving tomcat logs into the build folder
+		mkdir -p "$WORKSPACE/logs/${BUILD_NUMBER}"
+		mv "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build/tests/logs"/* "$WORKSPACE/logs/${BUILD_NUMBER}/"
+		rm -r "$WORKSPACE/${GIT_BRANCH_NAME}/dotCMS/build/tests/logs/"
 
-	#Removes old logs folders, preserving the first 20 (most recent)
-	cd "$WORKSPACE/logs/"
-	ls -dt */ | tail -n +21 | xargs rm -rf
+		#Removes old logs folders, preserving the first 20 (most recent)
+		cd "$WORKSPACE/logs/"
+		ls -dt */ | tail -n +21 | xargs rm -rf
+	fi
+else
+	echo "Nothing to do. Server is down"
 fi

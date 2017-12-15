e46d28ee684c910f8f2426d0cac34f4edc6b4c2c
Nollymar Longa <nollymar.longa@dotcms.com>
2017-12-15 12:21:32 -0500

    #dotCMS/core/12235 Setting conf needed to checkout current ee branch to run unit tests

--- jenkins_scripts/builds/unit/master_actual_build.sh
+++ jenkins_scripts/builds/unit/master_actual_build.sh
@@ -8,6 +8,20 @@ export GRADLE_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m"
 
 
 cd "$WORKSPACE/repo/dotCMS"
+
+current_branch=`git rev-parse --abbrev-ref HEAD`
+
+cd src/main/enterprise
+branch_exists=$(git ls-remote --heads git@github.com:dotCMS/enterprise-2.x.git $current_branch);
+
+echo $branch_exists
+
+if [[ $branch_exists ]]; then
+    (git fetch && git checkout $current_branch)
+    echo "Enterprise branch updated"
+    cd "$WORKSPACE/repo/dotCMS"
+fi
+
 sed -i "s,^org.gradle.jvmargs=,#org.gradle.jvmargs=,g" gradle.properties
 
 ./gradlew clean --no-daemon
